ee9fe7c54a1e0c48da2ff1b65ccd722a14a5fcc5
nbns <nickolay.bns@gmail.com>
2018-10-05 13:07:47 +0300

    extract TableOptions

--- src/simte/Table/ITableBuilder.cs
+++ src/simte/Table/ITableBuilder.cs
@@ -4,16 +4,6 @@ using System.Collections.Generic;
 
 namespace simte.Table
 {
-    public class TableOptions
-    {
-        public Position TopLeft { get; set; }
-        public bool Border { get; set; }
-        public bool Autofit { get; set; }
-
-        public Position? FreezePane { get; set; }
-        public bool WrapText { get; set; }
-    }
-
     public interface ITableBuilder : IAttachable<IExcelPackage>
     {
         TableOptions Options { get; }
---
+++ src/simte/Table/TableOptions.cs
@@ -0,0 +1,12 @@
+﻿namespace simte.Table
+{
+    public class TableOptions
+    {
+        public Position TopLeft { get; set; }
+        public bool Border { get; set; }
+        public bool Autofit { get; set; }
+
+        public Position? FreezePane { get; set; }
+        public bool WrapText { get; set; }
+    }
+}
